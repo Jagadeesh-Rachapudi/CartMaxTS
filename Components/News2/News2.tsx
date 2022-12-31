4db75015878cb2e79f16df42cd6564cbba41382b
Jagadeesh <rachapudijagadeesh9580@gmail.com>
2022-12-31 16:08:51 +0530

    Updated Shoes

--- Components/News2/News2.tsx
+++ Components/News2/News2.tsx
@@ -7,15 +7,11 @@ import NewsArticle2, {
   NewsArticle2Props,
 } from "../../Utils/NewsArticle2/NewsArticle2";
 import NewsArticle2Data from "../../Utils/NewsArticle2/NewsArticle2.json";
+import RedButton from "../../Utils/RedButton/RedButton";
 export interface IProps {
-  src: string;
-  src2: string;
-  title: string;
   tag: string;
-  imageText: string;
-  Button: string;
-  Button2: string;
-  Holiday: string;
+  title: string;
+  ButtonText: string;
 }
 
 function News2(props: IProps) {
@@ -23,14 +19,26 @@ function News2(props: IProps) {
     <Container className="News2-Body">
       <Row>
         <Col>
-          <div className="tag">JACKET & COAST</div>
-          <div className="title">
-            Get Cartmax now and set up your Store Today
-          </div>
+          <div className="tag"></div>
+          <div
+            className="tag"
+            dangerouslySetInnerHTML={{
+              __html: props.tag,
+            }}
+          />
+          <div
+            className="title"
+            dangerouslySetInnerHTML={{
+              __html: props.title,
+            }}
+          />
           <div className="Container-Box">
             <NewsArticle2 {...(NewsArticle2Data as NewsArticle2Props)} />
             <NewsArticle2 {...(NewsArticle2Data as NewsArticle2Props)} />
             <NewsArticle2 {...(NewsArticle2Data as NewsArticle2Props)} />
+          </div>
+          <div className="Button-Container mb-5 ">
+            <RedButton text={props.ButtonText} />
           </div>
         </Col>
       </Row>
