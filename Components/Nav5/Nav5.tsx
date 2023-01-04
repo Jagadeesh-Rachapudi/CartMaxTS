d5ec42c11d3b74379b3c8efee708ab68f595f24e
Jagadeesh <rachapudijagadeesh9580@gmail.com>
2023-01-04 12:21:46 +0530

    Shoes mini

--- Components/Nav5/Nav5.tsx
+++ Components/Nav5/Nav5.tsx
@@ -27,8 +27,16 @@ function Nav5(props: IProps) {
   return (
     <div className="Nav5-Body">
       <Container fluid="xl">
-        <Row className="d-flex flex-row justify-content-evenly">
-          <Col className="navbar2">
+        <Row className="d-flex flex-row justify-content-between">
+          <Col
+            className="navbar2 nav2"
+            xs={1}
+            sm={1}
+            md={1}
+            lg={1}
+            xl={1}
+            xxl={1}
+          >
             <Navbar
               bg="light"
               variant="dark"
@@ -80,7 +88,7 @@ function Nav5(props: IProps) {
               </Container>
             </Navbar>
           </Col>
-          <Col>
+          <Col className="col2">
             <Dropdown>
               <Dropdown.Toggle id="dropdown-basic" className="DropDownButton">
                 <AiOutlineMenu className="me-2" />
@@ -105,7 +113,7 @@ function Nav5(props: IProps) {
               bg="light"
               variant="dark"
               expand="lg"
-              className="bg-transparent nav-Links nav2  "
+              className="bg-transparent nav-Links nav1  "
             >
               <Container>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
