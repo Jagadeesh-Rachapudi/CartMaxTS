f948a74e4dbe7fed717af920627b2f4ba8fdb13d
Jagadeesh <rachapudijagadeesh9580@gmail.com>
2023-01-16 10:12:20 +0530

    Updated

--- Components/Details/Details.tsx
+++ Components/Details/Details.tsx
@@ -24,12 +24,12 @@ export interface IProps {
 function Details(props: IProps) {
   const [active, setActive] = useState("18k");
   return (
-    <Container className="Details-Body" fluid="xl">
+    <Container className="Details-Body" fluid="xxl">
       <Row>
-        <Col xs={12} sm={12} md={12} lg={4} xl={4} xxl={4}>
+        <Col xs={12} sm={12} md={12} lg={12} xl={4} xxl={4}>
           <div className="content">
             <div className="tag">SUMMER 2022</div>
-            <div className="title">Diamond Vine Climber Earrings</div>
+            <div className="title">Diamond Gold Chain</div>
             <div className="price">
               <b>$1,800.</b>99
             </div>
@@ -40,7 +40,7 @@ function Details(props: IProps) {
               tEditor for you so forat you can for you so you can.
             </div>
             <hr className="horizontalline" />
-            <div className="d-flex">
+            <div className="d-flex icons ">
               <div className="icon-container">
                 <BsFacebook size={17} color="#C2C2C2" className="icon" />
               </div>
@@ -53,17 +53,47 @@ function Details(props: IProps) {
             </div>
           </div>
         </Col>
-        <Col xs={12} sm={12} md={12} lg={4} xl={4} xxl={4}>
-          <div className="roseBox"></div>
-          <div className="d-flex justify-content-between mt-4">
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
+        <Col xs={12} sm={12} md={12} lg={12} xl={4} xxl={4}>
+          <div className="d-flex roseBox justify-content-center">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+              alt=""
+              className="img1"
+            />
+          </div>
+          <div className=" boxes d-flex justify-content-between mt-4">
+            <div className="box d-flex justify-content-center ">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
+            <div className="box d-flex justify-content-center ">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
+            <div className="box d-flex justify-content-center ">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
+            <div className="box d-flex justify-content-center ">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
           </div>
         </Col>
-        <Col xs={12} sm={12} md={12} lg={4} xl={4} xxl={4}>
-          <div className="reviews mt-5 mb-3 d-flex justify-content-between w-75 ">
+        <Col xs={12} sm={12} md={12} lg={12} xl={4} xxl={4}>
+          <div className="reviews mt-5 mb-3 d-flex justify-content-between  ">
             <div className="d-flex">
               <AiTwotoneStar size={17} color="gold" className="" />
               <AiTwotoneStar size={17} color="gold" className="" />
@@ -73,16 +103,34 @@ function Details(props: IProps) {
             </div>
             <div className="Count">4 Review(s)</div>
           </div>
-          <hr className="w-75 mb-2" />
+          <hr className="mb-2 horiLine2 " />
           <div className="hedding mb-1">MORE DESIGN</div>
-          <div className="d-flex justify-content-between w-75 mb-3">
-            <div className="box2"></div>
-            <div className="box2"></div>
-            <div className="box2"></div>
+          <div className="d-flex justify-content-between mb-3 boxes2 ">
+            <div className="box2 d-flex justify-content-center">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
+            <div className="box2 d-flex justify-content-center">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
+            <div className="box2 d-flex justify-content-center">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/catmax2-36a5f.appspot.com/o/Furniture%2FLayer%2019%20(1).png?alt=media&token=4b025c15-e38b-40af-b59a-a70cadefb335"
+                alt=""
+                className="img2"
+              />
+            </div>
           </div>
-          <hr className="w-75 mb-2" />
+          <hr className="horiLine2 mb-2" />
           <div className="hedding mb-1">SELECT GOLD</div>
-          <div className="d-flex justify-content-between w-75">
+          <div className="buttonsGroup">
             <div className="droppdown">
               <Dropdown as={ButtonGroup}>
                 <Button className=" name-button bg-transparent" variant="light">
@@ -119,19 +167,21 @@ function Details(props: IProps) {
               <div className="whishlist-text">Save to Whishlist</div>
             </div>
           </div>
-          <hr className="w-75 mb-4" />
-          <button className="Button mb-4 ">
-            <BsBag size={17} color="white" className="icon" /> Add to Cart
-          </button>
-          <hr className="w-75 mb-4" />
-          <div className="tags w-75 ">
+          <hr className="horiLine2 mb-4" />
+          <div className="button-Container">
+            <button className="Button mb-4 ">
+              <BsBag size={17} color="white" className="icon" /> Add to Cart
+            </button>
+          </div>
+          <hr className="horiLine2 mb-4" />
+          <div className="tags  ">
             <div className="tag1 me-2">
               <b>SKU:</b> U2012
             </div>
-            <div className="tag1">
+            <div className="tag1 me-2 ">
               <b>TAGS</b> Hot, Women
             </div>
-            <div className="tag1">
+            <div className="tag1 me-2 ">
               <b>Categories:</b> Earrings
             </div>
           </div>
