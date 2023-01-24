d66b58d9600a698ae523671e9231da46dadb484a
Jagadeesh <rachapudijagadeesh9580@gmail.com>
2023-01-24 16:29:17 +0530

    Update

--- Utils/Fashion/Fashion.tsx
+++ Utils/Fashion/Fashion.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BsSearch, BsHeart, BsBag } from "react-icons/bs";
+
+import { useDispatch, useSelector } from "react-redux";
+import { updateCart, updatelist } from "../../redux/actions/cartActions";
+import { IStore } from "../../redux/store";
 
 export interface IProps {
   sale: string;
@@ -20,6 +24,13 @@ export interface IProps {
 }
 
 function Fashion(props: IProps) {
+  const [selected, setSelected] = useState(0);
+  const [added, setAdded] = useState(false);
+
+  const dispatch = useDispatch();
+  const cart = useSelector((state: IStore) => state.cart.products);
+  const list = useSelector((state: IStore) => state.list.list);
+
   return (
     <Container className="Fashion-Body m-2">
       <Row>
@@ -43,14 +54,79 @@ function Fashion(props: IProps) {
               <div className="icons">
                 <BsSearch color="black" size={17} className="icon" />
               </div>
-              <div className="icons">
+              <div
+                className="icons"
+                onClick={() => {
+                  if (selected === 3) setSelected(0);
+                  else setSelected(3);
+
+                  const add = () => {
+                    setAdded(!added);
+                    dispatch(
+                      updatelist({
+                        list: [
+                          ...list,
+                          {
+                            pId: props.prodid,
+                            productName: props.fashionttile,
+                            pPrice: props.price,
+                          },
+                        ],
+                      })
+                    );
+                  };
+                  const remove = () => {
+                    setAdded(!added);
+                    dispatch(
+                      updatelist({
+                        list: list.filter(
+                          (product) => product.pId !== props.prodid
+                        ),
+                      })
+                    );
+                  };
+                  if (added == false) {
+                    add();
+                  } else if (added == true) {
+                    remove();
+                  }
+                }}
+              >
                 <BsHeart color="black" size={17} className="icon" />
               </div>
             </div>
           </div>
           <div className="showOnHover">
             <img src={props.img} alt="" className="img" />
-            <div className="cart">
+            <div
+              className="cart"
+              onClick={() => {
+                function addToCart(productId: number, newQuantity: number) {
+                  var existingProduct = cart.find(function (item) {
+                    return item.pId === productId;
+                  });
+
+                  if (existingProduct) {
+                    existingProduct.quantity += newQuantity;
+                  } else {
+                    dispatch(
+                      updateCart({
+                        products: [
+                          ...cart,
+                          {
+                            pId: props.prodid,
+                            productName: props.fashionttile,
+                            pPrice: props.price,
+                            quantity: newQuantity,
+                          },
+                        ],
+                      })
+                    );
+                  }
+                }
+                addToCart(props.prodid, 1);
+              }}
+            >
               <div className="ms-auto me-auto d-flex mt-3">
                 <BsBag color="white" size={17} className="bagicon mt-1" />
                 <div
